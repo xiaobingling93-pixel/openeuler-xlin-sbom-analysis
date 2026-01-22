8af45b1241aadb89738492f0fbe279fc2ea22eff
tianwei-li <dev13204@linx-info.com>
2026-01-22 13:37:13 +0800

    初始化软件仓库源扫描模块

---
+++ actions/scanner/repo_helper.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2025 Linx Software, Inc.
+#
+# xlin-sbom-analysis tool is licensed under Mulan PSL v2.
+
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#
+# http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+def _scan_primary_xml():
+    """
+    扫描并解析primary.xml压缩文件，提取软件包信息、许可证和漏洞数据
+    """
+
+
+def _scan_repomd():
+    """
+    扫描并解析repomd.xml文件，提取primary数据的URL和时间戳
+    """
+
+
+def _extract_primary_xml():
+    """
+    从给定的文本中提取primary.xml及其后续内容
+    """
+
+def scan_repo():
+    """
+    扫描单个源代码仓库，生成安全引入评估报告。
+    """
+
