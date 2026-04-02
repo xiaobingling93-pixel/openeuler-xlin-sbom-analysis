b648e32df3d0fcb6ede345075b107800bdb08ac7
tianwei-li <dev13204@linx-info.com>
2026-04-02 14:50:40 +0800

    初始化批量扫描报告生成模块

---
+++ actions/reporter/docx_reporter_pkg.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2025 Linx Software, Inc.
+#
+# xlin-sbom-analysis tool is licensed under Mulan PSL v2.
+
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#
+# http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+def _generate_dep_license_section_docx():
+    """
+    生成许可证分析部分的DOCX内容
+    """
+
+def _generate_license_section_docx():
+    """
+    生成许可证分析部分的DOCX内容
+    """
+
+def _generate_dep_vulnerability_table_docx():
+    """
+    生成漏洞信息表格并添加到DOCX文档中
+    """
+
+def _generate_vulnerability_table_docx():
+    """
+    生成软件包漏洞信息表格并添加到DOCX文档中
+    """
+
+def generate_docx_report():
+    """
+    生成DOCX格式的扫描报告
+    """
