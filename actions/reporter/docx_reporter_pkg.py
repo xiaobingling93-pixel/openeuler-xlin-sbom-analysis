25844de2bd0f9e148cd96a90133f3056818a6245
albert-leo <dev13401@linx-info.com>
2026-04-03 15:13:50 +0800

    新增批量扫描模式下软件包漏洞信息表格生成功能
    Signed-off-by: albert-leo <dev13401@linx-info.com>

--- actions/reporter/docx_reporter_pkg.py
+++ actions/reporter/docx_reporter_pkg.py
@@ -27,10 +27,48 @@ def _generate_dep_vulnerability_table_docx():
     生成漏洞信息表格并添加到DOCX文档中
     """
 
-def _generate_vulnerability_table_docx():
+def _generate_vulnerability_table_docx(doc, package):
     """
     生成软件包漏洞信息表格并添加到DOCX文档中
+
+    Args:
+        doc (Document): python-docx文档对象，用于添加表格内容
+        package (Package): 包含漏洞信息的Package对象
+
+    Returns:
+        None: 该函数直接修改传入的doc对象，不返回任何值
     """
+
+    # 创建表格
+    table = doc.add_table(rows=1, cols=4)
+    table.style = 'Table Grid'
+
+    # 添加表头
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = '漏洞ID'
+    hdr_cells[1].text = '漏洞评分标准'
+    hdr_cells[2].text = '漏洞评分'
+    hdr_cells[3].text = '修复版本'
+
+    # 应用表头样式
+    for cell in hdr_cells:
+        for paragraph in cell.paragraphs:
+            paragraph.style = '表格标题'
+
+    # 填充表格数据
+    for vuln in package.vulnerabilities:
+        row_cells = table.add_row().cells
+        row_cells[0].text = vuln['id']
+        row_cells[1].text = vuln.get('severity_type', 'N/A')
+        row_cells[2].text = vuln.get('severity_level', 'N/A')
+        row_cells[3].text = vuln.get('fixed', 'N/A')
+
+        # 应用表格内容样式
+        for cell in row_cells:
+            for paragraph in cell.paragraphs:
+                paragraph.style = '表格内容'
+
+    doc.add_paragraph()
 
 def generate_docx_report():
     """
